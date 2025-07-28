e8b5fdbad32a29969b7fcdbb513246d3d933cd93
VanshAgrawal <Vansh.Agrawal@Geminisolutions.com>
2025-07-28 13:14:50 +0530

    updates

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -24,7 +24,7 @@ const authOptions = {
           
           const isUserExists = await User.findOne({ email });
           if (!isUserExists) {
-            const res = await fetch("http://localhost:3000/api/user", {
+            const res = await fetch(`${process.env.NEXTAUTH_URL}/api/user`, {
               method: "POST",
               headers: {
                 "Content-Type": "application/json",
